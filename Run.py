3d972e3d64207261856235baf9808387fd02a5f9
Iangel <161000766+Iang3l@users.noreply.github.com>
2024-02-23 14:45:59 +0800

    Add files via upload

---
+++ Run.py
@@ -0,0 +1,4 @@
+from App import Template
+
+if __name__ == '__main__':
+    Template.run(debug=True)
---
+++ routes.py
@@ -0,0 +1,18 @@
+from flask import render_template, redirect, url_for
+from app import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/calendar')
+def calendar():
+    return render_template('calendar.html')
