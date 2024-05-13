12695b5e23ae88a34b10a0ce9cd14fade9c20b15
kyowa <157347453+callmejokes@users.noreply.github.com>
2024-05-13 15:45:50 +0800

    Update doctors.php

--- doctor/doctors.php
+++ doctor/doctors.php
@@ -89,7 +89,7 @@
                 </tr>
                 <tr class="menu-row" >
                     <td class="menu-btn menu-icon-patient">
-                        <a href="patient.php" class="non-style-link-menu"><div><p class="menu-text">Patients</p></a></div>
+                        <a href="patient.php" class="non-style-link-menu"><div><p class="menu-text">Students</p></a></div>
                     </td>
                 </tr>
 
